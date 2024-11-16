fe6d4b19c71b2283db61b7a07f40306a77cc0666
igorKornya <152435887+igorKornya@users.noreply.github.com>
2024-11-16 19:40:06 +0300

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -8,66 +8,81 @@ struct TNode {
     TNode* left;
     TNode* right;
     TNode* parent;
-    TNode(int _data) { data = _data; left = nullptr; right = nullptr; parent = nullptr; }
+
+    TNode(int _data) {
+        data = _data;
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
+    }
 };
 
 class BinaryThree {
-
 public:
     TNode* root;
+
     BinaryThree() { root = nullptr; }
     BinaryThree(int k) { root = new TNode(k); }
 
-    void addLeft(TNode* curr, int new_k) {
+    TNode* addLeft(TNode* curr, int new_k) {
         if (curr->left == nullptr) {
             TNode* new_l_node = new TNode(new_k);
             curr->left = new_l_node;
             new_l_node->parent = curr;
+            return new_l_node;
         }
+        return curr->left;
     }
 
-    void addRight(TNode* curr, int new_k) {
+    TNode* addRight(TNode* curr, int new_k) {
         if (curr->right == nullptr) {
             TNode* new_r_node = new TNode(new_k);
             curr->right = new_r_node;
             new_r_node->parent = curr;
+            return new_r_node;
         }
+        return curr->right;
     }
 
     void recursionInOrder(TNode* curr, vector<int>& res) {
-        if (curr != nullptr) {
-            recursionInOrder(curr->left, res);
-            res.push_back(curr->data);
-            recursionInOrder(curr->right, res);
-        }
+        if (curr == nullptr) return;
+        recursionInOrder(curr->left, res);
+        res.push_back(curr->data);
+        recursionInOrder(curr->right, res);
     }
 
     void recursionPreOrder(TNode* curr, vector<int>& res) {
-        if (curr != nullptr) {
-            res.push_back(curr->data);
-            recursionPreOrder(curr->left, res);
-            recursionPreOrder(curr->right, res);
-        }
+        if (curr == nullptr) return;
+        res.push_back(curr->data);
+        recursionPreOrder(curr->left, res);
+        recursionPreOrder(curr->right, res);
     }
 
     void recursionPostOrder(TNode* curr, vector<int>& res) {
-        if (curr != nullptr) {
-            recursionPostOrder(curr->left, res);
-            recursionPostOrder(curr->right, res);
-            res.push_back(curr->data);
-        }
+        if (curr == nullptr) return;
+        recursionPostOrder(curr->left, res);
+        recursionPostOrder(curr->right, res);
+        res.push_back(curr->data);
     }
 
     void printInOrder() {
+        if (root == nullptr) {
+            cout << "Tree is empty." << endl;
+            return;
+        }
         vector<int> res;
         recursionInOrder(root, res);
         for (int i : res) {
-            cout << i << " ";
+            cout << i;
         }
         cout << endl;
     }
 
     void printPreOrder() {
+        if (root == nullptr) {
+            cout << "Tree is empty." << endl;
+            return;
+        }
         vector<int> res;
         recursionPreOrder(root, res);
         for (int i : res) {
@@ -77,6 +92,10 @@ public:
     }
 
     void printPostOrder() {
+        if (root == nullptr) {
+            cout << "Tree is empty." << endl;
+            return;
+        }
         vector<int> res;
         recursionPostOrder(root, res);
         for (int i : res) {
@@ -84,24 +103,12 @@ public:
         }
         cout << endl;
     }
+
+    TNode* get_root() {
+        return root;
+    }
+
+    TNode* getParent(TNode* node) {
+        return node != nullptr ? node->parent : nullptr;
+    }
 };
-
-int main() {
-/*
-    BinaryThree tree(6);
-    tree.addLeft(tree.root, 2);
-    tree.addRight(tree.root, 7);
-    tree.addLeft(tree.root->left, 1);
-    tree.addRight(tree.root->left, 4);
-    tree.addRight(tree.root->right, 9);
-    tree.addLeft(tree.root->left->right, 3);
-    tree.addRight(tree.root->left->right, 5);
-    tree.addLeft(tree.root->right->right, 8);
-
-    cout << "In-order: "; tree.printInOrder();
-    cout << "Pre-order: "; tree.printPreOrder();
-    cout << "Post-order: "; tree.printPostOrder();*/
-
-    return 0;
-}
-
